59c407548279773cda0416f825496fd757106685
Hua <2272198978@qq.com>
2025-04-03 19:27:14 +0800

    feat: 技能剑投掷

---
+++ Assets/Script/Skills/Sword_Skill.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword_Skill : Skill
+{
+    [Header("Skill info")]
+    [SerializeField] private GameObject swordPrefab;
+    [SerializeField] private Vector2 launchDir;
+    [SerializeField] private float swordGravity;
+
+    public void CreateSword()
+    {
+        GameObject newSword = Instantiate(swordPrefab, player.transform.position, player.transform.rotation);
+        Sword_Skill_Controller newSwordScript = newSword.GetComponent<Sword_Skill_Controller>();
+        newSwordScript.SetupSword(launchDir, swordGravity);
+    }
+}
