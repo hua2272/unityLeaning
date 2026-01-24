42e49997b06026707547354dad9599bb31c12c3c
Hua <2272198978@qq.com>
2026-01-24 12:34:45 +0800

    feat: 新增测试脚本用于测试不同场景的功能

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadSceneAsync("Harbor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
